21c4bcc5dc95dd236ddf42a5d0ea83951e7ce5f7
Rizky Maulana <rizki.maulana@happyfresh.com>
2022-08-30 09:20:54 +0700

    Change version

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	gomodtest "github.com/rizkykojek/go-mod-test/v2"
+	"github.com/rizkykojek/go-mod-test/v2/helper"
 )
 
 func main() {
-	fmt.Print(gomodtest.SayHello("kojek"))
+	fmt.Print(helper.SayHello("kojek"))
 }
